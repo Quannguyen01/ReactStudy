8384fec404fbf7bf34b194a784216f22f9ab0f29
Quan Nguyen <quannguyenhong.92@gmail.com>
2016-04-14 00:16:59 -0500

    Adds in Avatar component

--- app/index.js
+++ app/index.js
@@ -1,12 +1,53 @@
+var USER_DATA = {
+  name: 'Quan Nguyen',
+  username: 'quannguyen01',
+  image: 'https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function(){
-    return(
-      <div>Hello Reactjs Program</div>
-    )
+    return <img src={this.props.imageUrl} style={{height: 100, width:100}} />
   }
-})
+});
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+var ProfileLink = React.createClass({
+  render:function(){
+    return (
+      <div>
+        <a href={'https://github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    );
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Avatar user={USER_DATA}/>,
+  document.getElementById('app')
+)
